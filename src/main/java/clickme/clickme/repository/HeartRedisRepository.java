fc01c4191fc22f4afbeff61aa2d2d345a03e0642
Seungrae <obey1342@gmail.com>
2023-09-19 23:07:50 +0900

    refactor: 중복코드 분리

--- src/main/java/clickme/clickme/repository/HeartRedisRepository.java
+++ src/main/java/clickme/clickme/repository/HeartRedisRepository.java
@@ -1,6 +1,7 @@
 package clickme.clickme.repository;
 
 import clickme.clickme.config.RedisConnectionCondition;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,24 +10,31 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @Conditional(RedisConnectionCondition.class)
-@RequiredArgsConstructor
 public class HeartRedisRepository implements HeartRepository {
 
     private final RedisTemplate<String, Long> redisTemplate;
+    private ValueOperations<String, Long> valueOperations;
+
+    public HeartRedisRepository(RedisTemplate<String, Long> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @PostConstruct
+    private void init() {
+        valueOperations = redisTemplate.opsForValue();
+    }
 
     @Override
     public void increaseCount(String id) {
-        ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
         valueOperations.increment(id);
     }
 
     @Override
     public void add(String id) {
-        redisTemplate.opsForValue().set(id, 0L);
+        valueOperations.set(id, 0L);
     }
 
     public Long findById(String id) {
-        ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
         Long count = valueOperations.get(id);
         return count == null ? 0L : count;
     }
