9b3c88a38a9cc503de4f6a3a4234fc5e18aeae16
Seungrae <obey1342@gmail.com>
2023-10-22 23:56:19 +0900

    chore: batch코드PR 병합

--- src/main/java/clickme/clickme/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package clickme.clickme.repository;
-
-import clickme.clickme.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByNickname(final String nickname);
-}
