53cf3085c88538045b721ecfa582428fc31d67be
Seungrae <obey1342@gmail.com>
2024-03-27 15:32:45 +0900

    feat: testcontainer로 mysql, redis설정 추가

---
+++ transfer-service/src/test/java/clickme/transferservice/job/member/AbstractIntegrationTest.java
@@ -0,0 +1,41 @@
+package clickme.transferservice.job.member;
+
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+
+import java.time.format.DateTimeFormatter;
+
+@Sql("/init.sql")
+@SpringBatchTest
+public abstract class AbstractIntegrationTest {
+
+    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    static final MySQLContainer MY_SQL_CONTAINER;
+    static final GenericContainer<?> REDIS;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest")
+                .withUsername("username")
+                .withPassword("password")
+                .withDatabaseName("clickme");
+        MY_SQL_CONTAINER.start();
+
+        REDIS = new GenericContainer<>("redis:latest")
+                .withExposedPorts(6379)
+                .withReuse(true);
+        REDIS.start();
+    }
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", REDIS::getHost);
+        registry.add("spring.data.redis.port", () -> "" + REDIS.getMappedPort(6379));
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+    }
+}
