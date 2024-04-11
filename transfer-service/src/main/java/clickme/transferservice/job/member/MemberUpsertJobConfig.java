dec3586066055e77b4da4158ac4591001e288412
Seungrae <obey1342@gmail.com>
2024-04-11 23:20:34 +0900

    feat: spring batch 파티셔닝 적용

--- transfer-service/src/main/java/clickme/transferservice/job/member/MemberUpsertJobConfig.java
+++ transfer-service/src/main/java/clickme/transferservice/job/member/MemberUpsertJobConfig.java
@@ -2,16 +2,16 @@ package clickme.transferservice.job.member;
 
 import clickme.transferservice.job.member.dto.DailyClickCount;
 import clickme.transferservice.job.member.dto.UpsertMember;
-import clickme.transferservice.repository.DailyClickRepository;
 import clickme.transferservice.repository.HeartRepository;
 import clickme.transferservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
@@ -23,14 +23,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.data.util.Pair;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @ConditionalOnProperty(value = "spring.batch.job.name", havingValue="syncRedisToMysqlJob")
@@ -44,28 +48,42 @@ public class MemberUpsertJobConfig  {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final MemberRepository memberRepository;
-    private final DailyClickRepository dailyClickRepository;
     private final HeartRepository heartRepository;
     private final MemberUpsertAfterJobListener memberUpsertAfterJobListener;
 
     @Bean
-    @StepScope
-    public ItemStreamReader<String> reader() {
-        final String key = REDIS_KEY.formatted(LocalDateTime.now().format(formatter));
-        return new RedisPagingItemReader(key, redisTemplate);
+    public TaskExecutor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(16);
+        executor.setMaxPoolSize(16);
+        executor.setThreadNamePrefix("partition-thread");
+        executor.setWaitForTasksToCompleteOnShutdown(Boolean.TRUE);
+        executor.initialize();
+        return executor;
     }
 
     @Bean
     @StepScope
-    public ItemProcessor<String, Pair<UpsertMember, DailyClickCount>> processor(
+    public ItemStreamReader<TypedTuple<String>> reader(
+            @Value("#{stepExecutionContext[startOffset]}") Long startOffset,
+            @Value("#{stepExecutionContext[endOffset]}") Long endOffset
+    ) {
+        final String key = REDIS_KEY.formatted(LocalDateTime.now().format(formatter));
+        return new RedisPagingItemReader(key, redisTemplate, startOffset, endOffset);
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<TypedTuple<String>, Pair<UpsertMember, DailyClickCount>> processor(
             @Value("#{jobParameters['createAt']}") final String createAt
     ) {
-        return name -> {
+        return tuple -> {
+            final String name = tuple.getValue();
             Long clickCount = heartRepository.getClickCount(name);
-            Long dailyClickCount = dailyClickRepository.getClickCount(name);
+
             return Pair.of(
                     new UpsertMember(name, clickCount),
-                    new DailyClickCount(name, LocalDate.parse(createAt), dailyClickCount)
+                    new DailyClickCount(name, LocalDate.parse(createAt), tuple.getScore().longValue())
             );
         };
     }
@@ -77,14 +95,13 @@ public class MemberUpsertJobConfig  {
     }
 
     @Bean
-    @JobScope
-    public Step syncRedisToMySqlStep(final ItemReader<String> reader,
+    public Step syncRedisToMySqlStep(final ItemReader<TypedTuple<String>> reader,
                                      final ItemWriter<Pair<UpsertMember, DailyClickCount>> writer,
-                                     final ItemProcessor<String, Pair<UpsertMember, DailyClickCount>> processor,
+                                     final ItemProcessor<TypedTuple<String>, Pair<UpsertMember, DailyClickCount>> processor,
                                      final JobRepository jobRepository,
                                      final PlatformTransactionManager transactionManager) {
         return new StepBuilder(STEP_NAME, jobRepository)
-                .<String, Pair<UpsertMember, DailyClickCount>>chunk(CHUCK_SIZE, transactionManager)
+                .<TypedTuple<String>, Pair<UpsertMember, DailyClickCount>>chunk(CHUCK_SIZE, transactionManager)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
@@ -92,11 +109,30 @@ public class MemberUpsertJobConfig  {
     }
 
     @Bean
-    public Job syncRedisToMysqlJob(@Qualifier("syncRedisToMySqlStep") final Step syncRedisToMysqlStep,
+    public Step masterStep(@Qualifier("syncRedisToMySqlStep") Step partitionStep,
+                           JobRepository jobRepository,
+                           PlatformTransactionManager transactionManager) {
+        return new StepBuilder("masterStep", jobRepository)
+                .partitioner("syncRedisToMySqlStep", partitioner())
+                .step(partitionStep)
+                .gridSize(10) // 파티션 수
+                .taskExecutor(executor())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public Partitioner partitioner() {
+        return new RedisRangePartitioner(REDIS_KEY.formatted(LocalDateTime.now().format(formatter)), 10, redisTemplate);
+    }
+
+
+    @Bean
+    public Job syncRedisToMysqlJob(@Qualifier("masterStep") final Step masterStep,
                                    final JobRepository jobRepository) {
         return new JobBuilder(JOB_NAME, jobRepository)
                 .incrementer(new RunIdIncrementer())
-                .flow(syncRedisToMysqlStep)
+                .flow(masterStep)
                 .end()
                 .listener(memberUpsertAfterJobListener)
                 .build();
--- transfer-service/src/main/java/clickme/transferservice/job/member/RedisPagingItemReader.java
+++ transfer-service/src/main/java/clickme/transferservice/job/member/RedisPagingItemReader.java
@@ -1,74 +1,48 @@
 package clickme.transferservice.job.member;
 
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-public class RedisPagingItemReader implements ItemStreamReader<String> {
+public class RedisPagingItemReader implements ItemStreamReader<TypedTuple<String>> {
 
     private static final int MEMBER_COUNT = 1000;
-    private static final double SCORE_INCREMENT = 1;
-    private static final double SCORE_MAX = Double.MAX_VALUE;
 
     private final String key;
     private final RedisTemplate<String, String> redisTemplate;
-    private double lastScore = 0;
-    private boolean hasReadBefore = false;
-    private Queue<String> items = new LinkedList<>();
+    private long startOffset = 0L;
+    private long endOffset = Long.MAX_VALUE;
+    private Queue<TypedTuple<String>> items = new LinkedList<>();
 
-    public RedisPagingItemReader(String key, RedisTemplate<String, String> redisTemplate) {
+    public RedisPagingItemReader(final String key,
+                                 final RedisTemplate<String, String> redisTemplate,
+                                 final long startOffset,
+                                 final long endOffset) {
         this.key = key;
         this.redisTemplate = redisTemplate;
+        this.startOffset = startOffset;
+        this.endOffset = endOffset;
     }
 
     @Override
-    public String read() {
+    public TypedTuple<String> read() {
         if (items.isEmpty()) {
             fetchNextPage();
         }
-        return items.poll();
+        return items.isEmpty() ?  null : items.poll();
     }
 
     private void fetchNextPage() {
-        Set<String> page = getItemsByScoreRange();
-        if (!page.isEmpty()) {
-            String lastItem = getLastItemFromPage(page);
-            updateLastScore(lastItem);
+        long nextPageEndOffset = Math.min(startOffset + MEMBER_COUNT - 1, endOffset);
+        Set<TypedTuple<String>> page = redisTemplate.opsForZSet().rangeWithScores(key, startOffset, nextPageEndOffset);
+        if (page != null && !page.isEmpty()) {
             items.addAll(page);
+            startOffset += page.size(); // 페이지를 읽은 후 offset 업데이트
         }
-    }
-
-    private Set<String> getItemsByScoreRange() {
-        double minScore = hasReadBefore ? lastScore + SCORE_INCREMENT : lastScore;
-        return redisTemplate.opsForZSet().rangeByScore(key, minScore, SCORE_MAX, 0, MEMBER_COUNT);
-    }
-
-    private String getLastItemFromPage(Set<String> page) {
-        return page.stream().reduce((first, second) -> second).orElse(null);
-    }
-
-    private void updateLastScore(String item) {
-        if (item != null) {
-            lastScore = redisTemplate.opsForZSet().score(key, item);
-            hasReadBefore = true;
-        }
-    }
-
-    @Override
-    public void open(ExecutionContext executionContext) {
-        if (executionContext.containsKey("lastScore")) {
-            lastScore = executionContext.getDouble("lastScore");
-            hasReadBefore = (boolean) executionContext.get("hasReadBefore");
-        }
-    }
-
-    @Override
-    public void update(ExecutionContext executionContext) {
-        executionContext.putDouble("lastScore", lastScore);
-        executionContext.put("hasReadBefore", hasReadBefore);
     }
 }
+
