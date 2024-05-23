18cd0e840ee86e9ea401cd1bfe973a24608fe67c
Seungrae <obey1342@gmail.com>
2024-05-23 23:42:42 +0900

    refactor: null일 경우 방어로직 추가

--- click-service/src/main/java/clickme/clickme/ranking/infrastructure/RankingRedisRepository.java
+++ click-service/src/main/java/clickme/clickme/ranking/infrastructure/RankingRedisRepository.java
@@ -5,12 +5,12 @@ import clickme.clickme.ranking.domain.RankingRepository;
 import clickme.clickme.ranking.infrastructure.dto.RankingDto;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
@@ -18,15 +18,11 @@ import java.util.concurrent.atomic.AtomicLong;
 public class RankingRedisRepository implements RankingRepository {
 
     private static final String RANKING_KEY = "clicks";
-    private static final String CHANGED_KEY = "clickCountChanged%s";
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     private final ZSetOperations<String, String> rankings;
-    private final SetOperations<String, String> changedMembers;
 
     public RankingRedisRepository(RedisTemplate<String, String> redisTemplate) {
         this.rankings = redisTemplate.opsForZSet();
-        this.changedMembers = redisTemplate.opsForSet();
     }
 
     @Override
@@ -47,15 +43,21 @@ public class RankingRedisRepository implements RankingRepository {
 
     @Override
     public Long findRankByName(final String name) {
-        return rankings.reverseRank(RANKING_KEY, name) + 1;
+        final Long rank = rankings.reverseRank(RANKING_KEY, name);
+        return rank == null ? 0L : rank + 1;
     }
 
     @Override
     public List<RankingDto> findLiveRanking(final int start, final int end) {
         final AtomicLong ranking = new AtomicLong(start + 1);
 
-        return rankings.reverseRangeWithScores(RANKING_KEY, start, end)
-                .stream()
+        Set<ZSetOperations.TypedTuple<String>> rangeWithScores
+                = rankings.reverseRangeWithScores(RANKING_KEY, start, end);
+        if (rangeWithScores == null) {
+            return Collections.emptyList();
+        }
+
+        return rangeWithScores.stream()
                 .map(tuple -> new RankingDto(ranking.getAndIncrement(), tuple.getValue(), tuple.getScore().longValue()))
                 .toList();
     }
