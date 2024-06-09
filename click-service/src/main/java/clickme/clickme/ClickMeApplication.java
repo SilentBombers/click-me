c30cd0fa530ab4e5cfda5725ab47e6d39a8f60eb
Seungrae <obey1342@gmail.com>
2024-06-09 22:32:25 +0900

    feat: 로깅 설정을 추가

--- click-service/src/main/java/clickme/clickme/ClickMeApplication.java
+++ click-service/src/main/java/clickme/clickme/ClickMeApplication.java
@@ -1,8 +1,10 @@
 package clickme.clickme;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ClickMeApplication {
 
