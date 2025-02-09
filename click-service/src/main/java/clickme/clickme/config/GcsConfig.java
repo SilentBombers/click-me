0413e9051de67698aca7ee66dafb283146fc40af
Seungrae Kim <obey1342@gmail.com>
2025-02-09 23:10:35 +0900

    refactor: storage 인증하는 방법 환경변수로 변경

--- click-service/src/main/java/clickme/clickme/config/GcsConfig.java
+++ click-service/src/main/java/clickme/clickme/config/GcsConfig.java
@@ -3,25 +3,19 @@ package clickme.clickme.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class GcsConfig {
 
-    @Value("${gcp.storage.credentials.location}")
-    private String credentialsPath;
-
     @Bean
-    public Storage storage(ResourceLoader resourceLoader) throws IOException {
-        InputStream credentialsStream = resourceLoader.getResource(credentialsPath).getInputStream();
+    public Storage storage() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
         return StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(credentialsStream))
+                .setCredentials(credentials)
                 .build()
                 .getService();
     }
